6ece37d6d441fe75336219282d4525a4a9f4cec2
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2022-12-22 23:13:25 +0100

    initial commit

---
+++ src/script/script.js
@@ -0,0 +1,81 @@
+const form = document.getElementById('form');
+const spinner = document.getElementById('spinner');
+const qr = document.getElementById('qrcode');
+
+
+// REGEX to validate URL
+const reg =  /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+const regex = new RegExp(reg);
+
+// Control Spinner
+const hideSpinner = () => {
+    spinner.style.display = 'none';
+}
+const runSpinner = () => {
+    spinner.style.display = 'block';
+}
+const spinnerRegulator = () => {
+    runSpinner();
+    setTimeout(() => {
+        hideSpinner();
+    }, 1500);
+}
+const generateCode = (url, size, theme) => {
+    const qrcode = new QRCode("qrcode", {
+        text: url,
+        width: size,
+        height: size
+    })
+}
+const clear = () => {
+    qr.innerHTML = '';
+
+    const btn = document.getElementById('save-link');
+    if (btn) btn.remove();
+}
+const createDownloadBtn = (downloadUrl) => {
+    const downloadBtn = document.createElement('a');
+    downloadBtn.id = 'save-link';
+    downloadBtn.classList = 'p-3 px-10 text-offWhite mx-auto mt-8 rounded-lg text-lg bg-red hover:bg-red-500';
+    downloadBtn.href = downloadUrl;
+    downloadBtn.download = 'QRcode';
+    downloadBtn.innerHTML = 'Download Image';
+    document.getElementById('generate').appendChild(downloadBtn);
+}
+
+hideSpinner();
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let url = document.getElementById('url').value;
+    const size = document.getElementById('size').value;
+    const alert = document.getElementById('alert');
+    
+    if (url === '') {
+        alert.style.display = 'inline';
+
+        setTimeout(() => {
+        alert.style.display = 'none'
+        }, 1000);
+    } else if(!url.match(regex)){ // REGEX validation
+        alert.innerHTML = 'Invalid URL';
+        alert.style.display = 'inline';
+        
+        setTimeout(() => {
+        alert.style.display = 'none'
+        }, 1000);
+    }
+     else {
+        spinnerRegulator();
+
+        setTimeout(() => {
+            clear();
+            generateCode(url, size);
+
+            setTimeout(() => {
+                const downloadUrl = qr.querySelector('img').src;
+                createDownloadBtn(downloadUrl);
+            }, 50);
+        }, 1500);
+    }
+})
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/script/*.js", "index.html"],
+  theme: {
+    fontFamily: {
+      'josefin': ['Josefin Sans', 'sans-serif'],
+      'manrope': ['Manrope', 'sans-serif']
+    },
+    colors: {
+      red: '#E63946',
+      offWhite: '#F1FAEE',
+      powderBlue: '#A8DADC',
+      grayishBlue: '#CCCCCC',
+      prussianBlue: '#1D3557',
+      oxfordBlue: '#14213D'
+    },
+    extend: {
+      width: {
+        '300': '500px'
+      }
+    },
+  },
+  plugins: [],
+}
